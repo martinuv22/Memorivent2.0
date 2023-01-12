01fc1fc1e324eedf8b8c120beee81e2acaa5ee05
Martin Utrera <utrera.martin90@gmail.com>
2023-01-12 00:58:48 -0300

    Detalles proyecto final

--- src/assets/firebase.js
+++ src/assets/firebase.js
@@ -1,5 +1,7 @@
+
+
 import { initializeApp } from "firebase/app";
-import {getFirestore, addDoc, getDocs, getDoc,  updateDoc, deleteDoc,collection, doc} from 'firebase/firestore'
+import {getFirestore, addDoc, getDocs, getDoc, updateDoc, deleteDoc, collection, doc} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
@@ -10,23 +12,16 @@ const firebaseConfig = {
   appId: "1:817776070304:web:cdc9f7a324d2dc29fcc3c5"
 };
 
-
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore()
+const db = getFirestore();
 
-/* metodo Crud
-C CREATE
-R READ
-U UPDATE
-D DELETE
-*/
 
-const cargarBDD = async () => {
-    const promise = await fetch('./json/productos.json')
-    const productos = await promise.json()
-    productos.forEach(async (prod) => {
-        await addDoc(collection(db,"productos"), { //collection si existe consulta si no existe crea
+const cargarBDD = async ()=>{
+    const promise = await fetch('./json/productos.json');
+    const productos = await promise.json();
+    productos.forEach(async (prod) =>{
+        await addDoc(collection(db,"productos"),{
             nombre: prod.nombre,
             marca: prod.marca,
             modelo: prod.modelo,
@@ -51,7 +46,7 @@ const getProducto =  async (id) => {
     const item = {...producto.data(), id: producto.id}
     return item
 }
-// 1 o 0
+
 const updateProducto = async (id, info) => {
     const estado = await updateDoc(doc(db,"productos", id), info)
     return estado
@@ -61,8 +56,6 @@ const deleteProducto = async(id) =>{
     const estado = await deleteDoc(doc(db, "productos", id))
     return estado
 }
-
-// ORDENES COMPRA crear y poder leerlas
 
 const createOrdenCompra = async (cliente, preTotal, fecha ) => {
     const ordenCompra = await addDoc(collection(db, "ordenCompra"),{
